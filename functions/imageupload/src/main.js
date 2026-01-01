90c633711bbee52deaedd32a6c403bc96008f305
hn-e <pigeon.himanshu@gmail.com>
2026-01-01 09:11:04 +0530

    image upload v2- signed url

--- functions/imageupload/src/main.js
+++ functions/imageupload/src/main.js
@@ -1,46 +1,41 @@
-import AWS from "aws-sdk";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export default async ({ req, res, log }) => {
   try {
-    log("Function started");
     const data = req.body ? JSON.parse(req.body) : {};
-    log("Received data:", data);
-    const { name, type, base64 } = data;
+    const { name, type, storageFolder } = data;
 
-    if (!name || !base64) {
-      log("No file provided");
-      return res.json({ error: "No file provided" });
+    if (!name || !type) {
+      return res.json({ error: "Missing name or type" });
     }
 
-    const s3 = new AWS.S3({
-    //   endpoint: process.env.R2_ENDPOINT,      // e.g., https://<account>.r2.cloudflarestorage.com
-    //   accessKeyId: process.env.R2_KEY,
-    //   secretAccessKey: process.env.R2_SECRET,
-      endpoint: 'https://0d1c7b3acd3ae12efb5b7027d4750332.r2.cloudflarestorage.com',      // e.g., https://<account>.r2.cloudflarestorage.com
-      accessKeyId: '016a1c9498c63716ddd460b3bf143636',
-      secretAccessKey: '69c665f3aeae5ec144620d3087e525f9e31ab11407a0d1c18319ff7ed69d60bf',
+    const client = new S3Client({
       region: "auto",
-      signatureVersion: "v4",
+      endpoint: process.env.R2_ENDPOINT,
+      credentials: {
+        accessKeyId: process.env.R2_ACCESSKEY,
+        secretAccessKey: process.env.R2_SECRETKEY,
+      },
     });
 
-    const buffer = Buffer.from(base64, "base64");
+    const key = `${storageFolder}/${Date.now()}-${name}`;
 
-    log("Uploading file to R2:", name);
-    await s3
-      .putObject({
-        Bucket: 'userbase',
-        Key: name,
-        Body: buffer,
-        ContentType: type,
-      })
-      .promise();
+    const command = new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET_EXTROBASE,
+      Key: key,
+      ContentType: type,
+    });
 
-    const fileUrl = `https://pub-bf39ce353cf24eae9273de931ddda57e.r2.dev/${name}`;
-    log("Upload successful. File URL:", fileUrl);
+    const uploadUrl = await getSignedUrl(client, command, {
+      expiresIn: 300,
+    });
 
-    return res.json({ ok: true, url: fileUrl });
+    const publicUrl = `${process.env.R2_PUBLICURL_EXTROBASE}/${key}`;
+
+    return res.json({ uploadUrl, publicUrl });
   } catch (err) {
-    log("Error:", err);
+    log(err);
     return res.json({ error: err.message });
   }
 };
